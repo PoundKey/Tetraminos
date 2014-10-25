fd288e99039efab25139458ebd098bf6afbfdb97
C.T Xue <xuecnt@hotmail.com>
2014-10-25 15:23:20 -0700

    initial setup with expressjs, angularjs next

--- Tetraminos/public/js/custom.js
+++ Tetraminos/public/js/custom.js
@@ -10,18 +10,18 @@ jQuery(document).ready(function($){
 	/***   ------------------------------------------------------   ***/
 	var options = {
 		startDate : new Date(),
-		endDate: new Date("February 31, 2014 11:13:00")
-		
+		endDate: new Date("November 17, 2014 21:00:00"),
+
 		/***   TRANSLATION : Enable following line by delete //   ***/
 		// ,titleDays : 'hari', titleHours : 'jam', titleMinutes : 'menit', titleSeconds : 'detik'
-		
+
 		/***   Video ID of youtube video - leave blank when wouldn't like to use youtube video as background   ***/
-		,videoID: ''
-	};	
-	
-	$('#container').bersua(options);	
+		videoID: ''
+	};
+
+	$('#container').bersua(options);
 	if(options.videoID){
 		$('body').tubular({videoId: options.videoID});
 	}
-	
+
 });
