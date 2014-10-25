775759c8232f4ae9faf93c67f2eb88f29e148b9b
C.T Xue <xuecnt@hotmail.com>
2014-10-25 16:54:42 -0700

    angularjs initial integration

---
+++ Tetraminos/public/app/controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('AppCtrl', ['$scope', '$location', '$http', '$routeParams',
+   function ($scope, $location, $http, $routeParams) {
+
+
+       
+   	
+   }]);
+
+
+
+
+
+
+
+
