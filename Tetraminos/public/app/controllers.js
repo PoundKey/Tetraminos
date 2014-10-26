4ac7f64d25e00b5a236f457a21c45285084280fc
C.T Xue <xuecnt@hotmail.com>
2014-10-25 20:47:02 -0700

    done for setting up the AJAX request thorugh api.js

--- Tetraminos/public/app/controllers.js
+++ Tetraminos/public/app/controllers.js
@@ -3,24 +3,46 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('AppCtrl', ['$scope', '$location', '$http', '$routeParams',
-   function ($scope, $location, $http, $routeParams) {
+    .controller('AppCtrl', ['$scope', '$location', '$http', '$routeParams',
+        function ($scope, $location, $http, $routeParams) {
 
-       $scope.cc = "The code analyzer area.";
-
-
-
-       $scope.marioAnalyzer = function () {
-           alert("Start analyzing Mario source code...");
-       };
-
-        $scope.pacmanAnalyzer = function () {
-           alert("Start analyzing Pacman source code...");
-       };
+            $scope.message = undefined;
 
 
-   	
-   }]);
+            $scope.marioAnalyzer = function () {
+
+                var msg = undefined;
+                $http.get('/api/mario')
+                    .success(function (data, status, headers, config) {
+                        msg = data;
+                        //alert("Start analyzing Mario source code: " + msg.title + " @ " + msg.content);
+                        $scope.message = "Start analyzing Mario source code...";
+                    })
+                    .error(function (data, status, headers, config) {
+                        alert("Error when sending the AJAX request. ");
+                        $scope.message = data;
+                    });
+
+
+            };
+
+            $scope.pacmanAnalyzer = function () {
+                var msg = undefined;
+                $http.get('/api/pacman')
+                    .success(function (data, status, headers, config) {
+                        msg = data;
+                        //alert("Start analyzing Pacman source code: " + msg.title + " @ " + msg.content);
+                        $scope.message = "Start analyzing Pacman source code...";
+                    })
+                    .error(function (data, status, headers, config) {
+                        alert("Error when sending the AJAX request. ");
+                        $scope.message = data;
+                    });
+                //alert("Start analyzing Pacman source code...");
+            };
+
+
+        }]);
 
 
 
--- Tetraminos/routes/api.js
+++ Tetraminos/routes/api.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.get( '/mario', function (req, res) {
+
+  res.json({title: 'Mario Analyzer', content: 'Dynamic invocation for Mario put here'});
+
+});
+
+router.get( '/pacman', function (req, res) {
+  res.json({title: 'Pacman Analyzer', content: 'Dynamic invocation for Pacman put here'});
+
+});
+
 module.exports = router;
