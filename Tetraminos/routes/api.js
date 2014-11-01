f070a40a0763213dd497f7d7649bea7d3742f3d7
C.T Xue <xuecnt@hotmail.com>
2014-10-31 23:39:59 -0700

    start cmd.exe with script running inside of the shell

--- Tetraminos/routes/api.js
+++ Tetraminos/routes/api.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var sys = require('sys');
 var exec = require('child_process').exec;
-var child;
+var execFile = require("child_process").execFile
+var child = undefined;
 
 /* GET users listing. */
 router.get('/', function (req, res) {
@@ -13,6 +14,7 @@ router.get('/mario', function (req, res) {
     //if (process.getuid) {
     //    console.log('Current uid: ' + process.getuid());
     //}
+    if (child != undefined) child.kill();
     var output = undefined;
     child = exec("./mario.out", {cwd: "static_lib"}, function (error, stdout, stderr) {
         //sys.print('stdout: ' + stdout);
@@ -30,6 +32,7 @@ router.get('/mario', function (req, res) {
 });
 
 router.get('/bejew', function (req, res) {
+    if (child != undefined) child.kill();
     var output = undefined;
     child = exec("Bejeweled.exe", {cwd: "static_lib/Games/Bejeweled"}, function (error, stdout, stderr) {
         console.log(stdout);
@@ -44,8 +47,9 @@ router.get('/bejew', function (req, res) {
 });
 
 router.get('/sod', function (req, res) {
+    if (child != undefined) child.kill();
     var output = undefined;
-    child = exec("SOD.exe", {cwd: "static_lib/Games/SOD"}, function (error, stdout, stderr) {
+    child = exec('start cmd.exe /k "SOD.exe"', {cwd: "static_lib/Games/SOD"}, function (error, stdout, stderr) {
         console.log(stdout);
         if (error !== null) {
             console.log('exec error: ' + error);
@@ -59,6 +63,7 @@ router.get('/sod', function (req, res) {
 
 
 router.get('/sudoku', function (req, res) {
+    if (child != undefined) child.kill();
     var output = undefined;
     child = exec("sudoku.exe", {cwd: "static_lib/Games/Sudoku"}, function (error, stdout, stderr) {
         console.log(stdout);
