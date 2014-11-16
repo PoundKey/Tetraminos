26d3d371fd78934da530e8ecb44e40e0a9884316
laves <ian_lavery@hotmail.com>
2014-11-15 16:43:24 -0800

    update Sonicizer and adding Linux OSC package
    - sonicizer has track number now

--- Sonicizer/InstrumentProfile.cpp
+++ Sonicizer/InstrumentProfile.cpp
@@ -14,6 +14,10 @@ void InstrumentProfile::setTrackTemplate(int fam) {
 	trackTemplate = fam;
 }
 
+void InstrumentProfile::setTrackNumber(int num) {
+	trackNumber = num;
+}
+
 void InstrumentProfile::setRegisterBooster(int boost) {
 	registerBooster = boost;
 }
@@ -25,18 +29,31 @@ void InstrumentProfile::setChannel(int chan) {
 string InstrumentProfile::getClassName() const {
 	return className;
 }
+
 int InstrumentProfile::getTrackTemplate(){
 	return trackTemplate;
 }
+
+int InstrumentProfile::getTrackNumber(){
+	return trackNumber;
+}
+
 int InstrumentProfile::getInheritanceFamily(){
 	return inheritanceFamily;
 }
+
 int InstrumentProfile::getRegisterBooster(){
 	return registerBooster;
 }
+
 int InstrumentProfile::getChannel(){
 	return channel;
 }
+
 map<string, int>& InstrumentProfile::getMethodToNoteMap() {
 	return methodToNoteMap;
+}
+
+void InstrumentProfile::putInMethodToNoteMap(string key, int val) {
+	methodToNoteMap[key] = val;
 }
--- Sonicizer/InstrumentProfile.h
+++ Sonicizer/InstrumentProfile.h
@@ -8,21 +8,25 @@ class InstrumentProfile {
 	std::map<string, int> methodToNoteMap;
 	int inheritanceFamily;
 	int trackTemplate;
+	int trackNumber;
 	int registerBooster;
 	int channel;
 
 public:
 	map<string, int>& getMethodToNoteMap();
+	void putInMethodToNoteMap(string, int);
 
 	string getClassName() const;
 	int getInheritanceFamily();
 	int getTrackTemplate();
+	int getTrackNumber();
 	int getRegisterBooster();
 	int getChannel();
 
 	void setClassName(string);
 	void setInheritanceFamily(int);
 	void setTrackTemplate(int);
+	void setTrackNumber(int);
 	void setRegisterBooster(int);
 	void setChannel(int);
 
