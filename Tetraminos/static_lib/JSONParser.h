8e3c7afb15052c034f8509b09201319b2824eaee
James Hicklin <hicklin.james@gmail.com>
2014-11-16 15:56:52 -0800

    Got Dane's refactored code to compile

--- Tetraminos/static_lib/JSONParser.h
+++ Tetraminos/static_lib/JSONParser.h
@@ -1,6 +1,5 @@
 #include "rapidjson/document.h"
 #include "rapidjson/filestream.h"
-#include "ClassProfile.cpp"
 #include <iostream>
 #include <map>
 #include <cstdio>
@@ -15,15 +14,23 @@
 #include <fstream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <iterator> 
+#include "DynamicParser.h"
 //#include <windows.h>
 //#include <tlhelp32.h>
-//#include "Sonifier/OSCmessenger.cpp"
-//#include "../../Fuser/DynamicAnalyser/DynamicRunner.h"
-
+#include "../../Sonicizer/OSCMessenger.h"
+//#include "../../Sonicizer/InstrumentProfile.h"
+#include "../../Fuser/DynamicAnalyser/DynamicRunner.h"
+#include "boost/date_time/posix_time/posix_time.hpp"
+using namespace std;
 class JSONParser
 {
 public:
-  int containsMatch(std::vector<std::vector<std::string> >, std::string, int&);
-  void addDependencies(ClassProfile::ClassProfile, std::map<std::string, ClassProfile::ClassProfile>&, std::set<std::string>&, std::vector<std::string>&, std::vector<std::string>&, std::vector<std::set<std::string> >&, std::set<int>&);
-  int getProcIdByName(string);
+	int containsMatch(std::vector<std::vector<std::string> >, std::string, int&);
+	void addDependencies(ClassProfile, std::map<std::string, ClassProfile>&, std::set<std::string>&, std::vector<std::string>&, std::vector<std::string>&, std::vector<std::set<std::string> >&, std::set<int>&);
+	int getProcIdByName(std::string);
+	void parseJSON(const rapidjson::Value&, std::vector<ClassProfile>&, std::map<std::string,ClassProfile>&, std::map<int, std::vector<ClassProfile> >&, int&, std::vector<std::string>&, std::map<std::string,std::string>&);
+	void createInstrumentCommands(OSCMessenger messenger);
+	std::vector<std::set<std::string> > constructDependency(std::map<std::string,ClassProfile>&, std::vector<std::string>&);
+	std::vector<std::vector<std::string> > constructInheritance(int, std::map<int, std::vector<ClassProfile> >);
 };
