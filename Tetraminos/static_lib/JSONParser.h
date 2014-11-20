336b8f72fca1b629421de844a612b1feb6bf007b
James Hicklin <hicklin.james@gmail.com>
2014-11-20 10:52:15 -0800

    Lots of comments for James' portion of JSONParser.cpp - also did a fair bit of refactoring for those parts

--- Tetraminos/static_lib/JSONParser.h
+++ Tetraminos/static_lib/JSONParser.h
@@ -30,7 +30,12 @@ public:
 	void addDependencies(ClassProfile, std::map<std::string, ClassProfile>&, std::set<std::string>&, std::vector<std::string>&, std::vector<std::string>&, std::vector<std::set<std::string> >&, std::set<int>&);
 	int getProcIdByName(std::string);
 	void parseJSON(const rapidjson::Value&, std::vector<ClassProfile>&, std::map<std::string,ClassProfile>&, std::map<int, std::vector<ClassProfile> >&, int&, std::vector<std::string>&, std::map<std::string,std::string>&);
-	void createInstrumentCommands(OSCMessenger messenger);
-	std::vector<std::set<std::string> > constructDependency(std::map<std::string,ClassProfile>&, std::vector<std::string>&);
-	std::vector<std::vector<std::string> > constructInheritance(int, std::map<int, std::vector<ClassProfile> >);
+	void createInstrumentCommands(OSCMessenger *messenger);
+	std::vector<std::set<std::string> >* constructDependency(std::map<std::string,ClassProfile>&, std::vector<std::string>&);
+	std::vector<std::vector<std::string> >* constructInheritance(int, std::map<int, std::vector<ClassProfile> >);
+private:
+	boost::posix_time::ptime getEarliestTimeStamp(std::map<std::string, std::vector<DynClassInfo> > *dynMap);
+	boost::posix_time::time_duration calcTimeDifference(boost::posix_time::ptime onTime, boost::posix_time::ptime offTime);
+	std::string extractFunctionNameFromString(std::string fullFuncName);
+	int checkTimeDurationForLessThanNSeconds(long totalTime);
 };
